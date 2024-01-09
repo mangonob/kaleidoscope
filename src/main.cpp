f774712196a2d816ea780ba22b3c190b79d431ff
mangonob <mangonob@qq.com>
2024-01-09 14:31:09 +0800

    WIP: args

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <sstream>
-#include <llvm/Support/Format.h>
+#include <argparse/argparse.hpp>
 #include "parser.tab.hpp"
 #include "TigerLexer.h"
 #include "absyn.h"
@@ -13,6 +13,32 @@ using namespace absyn;
 
 int main(int argc, char *argv[])
 {
+  argparse::ArgumentParser program("kalec", "1.0");
+  program.add_description("a tiger language compiler");
+
+  program.add_argument("input")
+      .remaining();
+
+  program.add_argument("-o", "--output")
+      .help("ouput filename")
+      .metavar("output")
+      .required();
+
+  program.add_argument("-emit-ir")
+      .help("emit llvm-ir")
+      .implicit_value(true)
+      .default_value(false);
+
+  try
+  {
+    program.parse_args(argc, argv);
+  }
+  catch (const exception &error)
+  {
+    cerr << program;
+    exit(1);
+  }
+
   yy::TigerLexer x;
   shared_ptr<Exp> exp;
   yy::TigerParser y(x, exp);
