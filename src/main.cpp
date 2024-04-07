c462c937c4ca44946b4a2a5c99c66d1a077e743f
mangonob <mangonob@qq.com>
2024-04-07 17:25:23 +0800

    WIP: move constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,53 @@
 #include <iostream>
-#include <memory>
-#include <sstream>
-#include <argparse/argparse.hpp>
-#include "parser.tab.hpp"
-#include "TigerLexer.h"
-#include "absyn.h"
-#include "codegen.h"
-#include "types.h"
 
 using namespace std;
-using namespace absyn;
+
+struct B
+{
+  B()
+  {
+  }
+};
+
+struct A
+{
+  A()
+  {
+    cout << "Default constructor" << endl;
+  }
+
+  A(const A &a)
+  {
+    cout << "Copy constructor" << endl;
+  }
+
+  A &operator=(const A &a)
+  {
+    cout << "Assign operator" << endl;
+    return *this;
+  }
+
+  A &operator=(const A &&a)
+  {
+    cout << "Move assign operator" << endl;
+    return *this;
+  }
+
+  A(const A &&a)
+  {
+    cout << "Move constructor" << endl;
+  }
+};
 
 int main(int argc, char *argv[])
 {
-  argparse::ArgumentParser program("kalec", "1.0");
-  program.add_description("a tiger language compiler");
-
-  program.add_argument("input")
-      .remaining();
-
-  program.add_argument("-o", "--output")
-      .help("ouput filename")
-      .metavar("output")
-      .required();
-
-  program.add_argument("-emit-ir")
-      .help("emit llvm-ir")
-      .implicit_value(true)
-      .default_value(false);
-
-  try
-  {
-    program.parse_args(argc, argv);
-  }
-  catch (const exception &error)
-  {
-    cerr << program;
-    exit(1);
-  }
-
-  yy::TigerLexer x;
-  shared_ptr<Exp> exp;
-  yy::TigerParser y(x, exp);
-
-  if (y.parse() == 0)
-  {
-    cg::CodeGenerator generator;
-    generator.generate(*exp);
-  }
-
-  exit(0);
+  A a;
+  A a2 = a;
+  A a3(a);
+  A a4 = std::move(a);
+  A a5(std::move(a));
+  a2 = a3;
+  a2 = std::move(a3);
+  std::swap(a2, a3);
+  exit(1);
 }
